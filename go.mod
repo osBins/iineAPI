36f2b807029dc0ff079703efd42eae3022c1921f
osbins <arpitjain.2001@gmail.com>
2022-11-19 23:54:15 +0530

    fetch random compliments on GET request

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module iineAPI
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,20 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home")
+	fmt.Fprintf(w, fetchCompliment())
+}
+
+var c []Compliments
+
+func fetchCompliment() string {
+	rand.Seed(time.Now().UnixNano())
+	idx := rand.Intn(cap(c))
+	return (c[idx].Compliment)
+}
+
+var data map[string]string
+
+type Compliments struct {
+	Id         string `json:"id"`
+	Compliment string `json:"compliment"`
 }
 
 func main() {
-	fmt.Println("Hello World")
+	jsonFile, err := os.Open("compliments.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(b, &data)
+	for key, element := range data {
+		c = append(c, Compliments{Id: key, Compliment: element})
+	}
+
+	fetchCompliment()
+	fmt.Println("Server Running")
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	log.Fatal(http.ListenAndServe(":8080", router))
+
 }
